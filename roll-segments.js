43411566f095f6511bfb3d2113a0bf4329a5153d
thomas-almeida <contato.thomasalmeidard@gmail.com>
2025-01-17 23:05:52 -0300

    LP basics

---
+++ roll-segments.js
@@ -0,0 +1,55 @@
+const allSegments = [
+  {
+    prefix: 'suas',
+    name: 'Roupas',
+    color: '#008181'
+  },
+  {
+    prefix: 'seus',
+    name: 'Achadinhos',
+    color: '#4303f5'
+  },
+  {
+    prefix: 'suas',
+    name: 'Ofertas',
+    color: '#ff0c0c'
+  },
+]
+
+function rollSegments() {
+  let prefix = document.querySelector('#prefix')
+  let name = document.querySelector('#name')
+
+  let currentIndex = 0 // Índice inicial
+  const interval = 2000 // Tempo em milissegundos
+  const totalSegments = allSegments.length // Total de segmentos
+
+  // Função para atualizar os elementos na página
+  function updateSegments() {
+    const segment = allSegments[currentIndex] // Segmento atual
+    prefix.innerHTML = segment.prefix // Atualiza o prefixo
+    name.innerHTML = segment.name // Atualiza o nome
+
+    name.style.color = segment.color
+
+    triggerAnimation(name);
+
+    currentIndex = (currentIndex + 1) % totalSegments // Próximo índice (volta ao início no final)
+  }
+
+  function triggerAnimation(element) {
+    element.classList.remove('fadeInUp-animation'); // Remove a classe
+    void element.offsetWidth; // Força o reflow para reiniciar a animação
+    element.classList.add('fadeInUp-animation'); // Adiciona novamente a classe
+  }
+
+  // Chama a função a cada intervalo
+  setInterval(updateSegments, interval)
+
+  // Opcional: imprime os nomes no console ao iniciar
+  allSegments.forEach((segment) => {
+    console.log(segment.name)
+  })
+}
+
+
